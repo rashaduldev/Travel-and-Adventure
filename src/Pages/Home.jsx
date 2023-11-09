b2612076871ee47ef4a2abd1f76a7bd0a59c3233
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-11-09 16:17:59 +0600

    Travel guide part done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,8 @@ import Banner from "../Component/Banner";
 import Destination from "../Component/Destination";
 import FAQsection from "./Home/FAQsection";
 import Featured from "./Home/Featured";
+import Leatestguide from "./Home/Leatestguide";
+import Testmonial from "./Home/Testmonial";
 import Tourtype from "./Home/Tourtype";
 import Whychosse from "./Home/Whychosse";
 
@@ -13,8 +15,10 @@ const Home = () => {
             <Tourtype></Tourtype>
             <Whychosse></Whychosse>
             <Destination></Destination>
-            <Featured></Featured>
             <FAQsection></FAQsection>
+            <Featured></Featured>
+            <Testmonial></Testmonial>
+            <Leatestguide></Leatestguide>
         </div>
     );
 };
---
+++ src/Pages/Home/Leatestguide.jsx
@@ -0,0 +1,143 @@
+const Leatestguide = () => {
+  return (
+    <div className="mx-10">
+      <div className="text-center my-9">
+        <h3 className="text-3xl italic">Updates</h3>
+        <h2 className="text-5xl font-bold">Leatest Travel Guide</h2>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+        <div className="bg-white border rounded-xl shadow-sm sm:flex dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+          <div className="flex-shrink-0 relative w-full rounded-t-xl overflow-hidden pt-[40%] sm:rounded-s-xl sm:max-w-[15rem] md:rounded-se-none md:max-w-xs">
+            <img
+              className="w-full h-full absolute top-0 start-0 object-cover"
+              src="https://i.ibb.co/tDpV8DW/1.jpg"
+              alt="Image Description"
+            />
+          </div>
+          <div className="flex flex-wrap">
+            <div className="p-4 flex flex-col h-full sm:p-7 space-y-5">
+              <div className="flex items-center justify-between">
+                <p className="mt-1 text-gray-500 dark:text-gray-400">
+                  2 july 2023
+                </p>
+                <li>admin</li>
+              </div>
+              <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+                The Empact of covid -19 on travels & tourism industry
+              </h3>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white border rounded-xl shadow-sm sm:flex dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+          <div className="flex-shrink-0 relative w-full rounded-t-xl overflow-hidden pt-[40%] sm:rounded-s-xl sm:max-w-[15rem] md:rounded-se-none md:max-w-xs">
+            <img
+              className="w-full h-full absolute top-0 start-0 object-cover"
+              src="https://i.ibb.co/RhNfnp2/2.jpg"
+              alt="Image Description"
+            />
+          </div>
+          <div className="flex flex-wrap">
+            <div className="p-4 flex flex-col h-full sm:p-7 space-y-5">
+              <div className="flex items-center justify-between">
+                <p className="mt-1 text-gray-500 dark:text-gray-400">
+                  2 july 2023
+                </p>
+                <li>admin</li>
+              </div>
+              <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+                The Empact of covid -19 on travels & tourism industry
+              </h3>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white border rounded-xl shadow-sm sm:flex dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+          <div className="flex-shrink-0 relative w-full rounded-t-xl overflow-hidden pt-[40%] sm:rounded-s-xl sm:max-w-[15rem] md:rounded-se-none md:max-w-xs">
+            <img
+              className="w-full h-full absolute top-0 start-0 object-cover"
+              src="https://i.ibb.co/Qc9Jr14/3.jpg"
+              alt="Image Description"
+            />
+          </div>
+          <div className="flex flex-wrap">
+            <div className="p-4 flex flex-col h-full sm:p-7 space-y-5">
+              <div className="flex items-center justify-between">
+                <p className="mt-1 text-gray-500 dark:text-gray-400">
+                  2 july 2023
+                </p>
+                <li>admin</li>
+              </div>
+              <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+                The Empact of covid -19 on travels & tourism industry
+              </h3>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white border rounded-xl shadow-sm sm:flex dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+          <div className="flex-shrink-0 relative w-full rounded-t-xl overflow-hidden pt-[40%] sm:rounded-s-xl sm:max-w-[15rem] md:rounded-se-none md:max-w-xs">
+            <img
+              className="w-full h-full absolute top-0 start-0 object-cover"
+              src="https://i.ibb.co/dWm7kFK/4.jpg"
+              alt="Image Description"
+            />
+          </div>
+          <div className="flex flex-wrap">
+            <div className="p-4 flex flex-col h-full sm:p-7 space-y-5">
+              <div className="flex items-center justify-between">
+                <p className="mt-1 text-gray-500 dark:text-gray-400">
+                  2 july 2023
+                </p>
+                <li>admin</li>
+              </div>
+              <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+                The Empact of covid -19 on travels & tourism industry
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr className="my-14" />
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-5 text-center mx-10 justify-center">
+        <div className="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+          <img
+            className="w-full h-full rounded-t-xl"
+            src="https://i.ibb.co/tZsfLzH/1.jpg"
+            alt="Image Description"
+          />
+        </div>
+        <div className="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7] mx-auto">
+          <img
+            className="w-full h-full rounded-t-xl"
+            src="https://i.ibb.co/vQ6pn5V/2.jpg"
+            alt="Image Description"
+          />
+        </div>
+        <div className="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7] mx-auto">
+          <img
+            className="w-full h-full rounded-t-xl"
+            src="https://i.ibb.co/ZmnjtZn/3.jpg"
+            alt="Image Description"
+          />
+        </div>
+        <div className="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7] mx-auto">
+          <img
+            className="w-full h-full rounded-t-xl"
+            src="https://i.ibb.co/vjvYjXN/4.jpg"
+            alt="Image Description"
+          />
+        </div>
+        <div className="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7] mx-auto">
+          <img
+            className="w-full h-full rounded-t-xl"
+            src="https://i.ibb.co/Czt2q1d/5.jpg"
+            alt="Image Description"
+          />
+        </div>
+       
+        
+      </div>
+    </div>
+  );
+};
+
+export default Leatestguide;
